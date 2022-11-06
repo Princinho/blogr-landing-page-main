40264d5b27469c74b795b7513ef3425da1e77c2f
Princinho <princegnakou@gmail.com>
2022-11-06 12:09:23 +0000

    add log to show clicks

--- scripts.js
+++ scripts.js
@@ -5,26 +5,29 @@ const closeNavImg=document.getElementById('img-close-nav')
 document.addEventListener('click', e => {
     const targetId = e.target.id
     const btnCloseNavIds = ['img-open-nav', 'img-close-nav', 'btn-toggle-nav']
-
-
+    console.log('clicked')
+    
     if (e.target.dataset.navToggle) {
+        console.log('toggled open nav')
         e.target.classList.toggle('opened')
     }
-
+    
     if (e.target.dataset.navToggle) {
         [...document.querySelectorAll('.sub-nav')]
-            .filter(subNav => subNav.id !== e.target.dataset.navToggle)
-            .map(subNav => {
-                subNav.classList.remove('visible')
-            })
+        .filter(subNav => subNav.id !== e.target.dataset.navToggle)
+        .map(subNav => {
+            subNav.classList.remove('visible')
+        })
         toggleSubNav(e.target.dataset.navToggle)
     }
-
+    
     if (btnCloseNavIds.find(e => e === targetId)) {
         if(document.body.classList.contains('open-nav')){
             openNavImg.classList.remove('hidden-nav-btn')
             closeNavImg.classList.add('hidden-nav-btn')
+            console.log('showing open nav icon')
         }else{
+            console.log('showing close nav icon')
             openNavImg.classList.add('hidden-nav-btn')
             closeNavImg.classList.remove('hidden-nav-btn')
         }
@@ -32,6 +35,7 @@ document.addEventListener('click', e => {
     }
 })
 function toggleSubNav(id) {
+    console.log('toggled open Sub nav')
     const subNav = document.getElementById(id)
     subNav.classList.toggle('visible')
 
